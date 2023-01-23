3fb1cd4be049d2cf89c142379f9e443a32fa4234
“dennohkim” <kimathidennis38@gmail.com>
2023-01-23 14:56:46 +0300

    feat: create sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ const Home: NextPage = () => {
       <Head>
         <title>T3Stack Blog Site</title>
       </Head>
-      <div className="flex h-screen w-full flex-col">
-        <header className="flex h-20   w-full flex-row items-center justify-around border-b-[1px] border-gray-300 bg-white ">
+      <div className="flex h-full w-full flex-col">
+        <header className="flex h-32   w-full flex-row items-center justify-around border-b-[1px] border-gray-300 bg-white ">
           <div>
             <IoReorderThreeOutline className="text-2xl text-gray-600" />
           </div>
@@ -80,22 +80,29 @@ const Home: NextPage = () => {
             </div>
             <div className="flex w-full flex-col justify-center space-y-7">
               {Array.from({ length: 5 }).map((_, i) => (
-                <div key={i} className="group flex flex-col space-y-6 pb-8 border-b border-gray-300 last:border-none ">
+                <div
+                  key={i}
+                  className="group flex flex-col space-y-6 border-b border-gray-300 pb-8 last:border-none "
+                >
                   <div className="flex w-full items-center space-x-10">
-                    <div className="w-10 h-10 bg-gray-400 rounded-full " ></div>
+                    <div className="h-10 w-10 rounded-full bg-gray-400 "></div>
                     <div>
-                    <p className="font-semibold ">Chizaa &#x2022; 22 Dec 2022</p>
-                    <p text-sm>Fullstack Designer | Software Developer | Blockchain Enthusiast</p>
+                      <p className="font-semibold ">
+                        Chizaa &#x2022; 22 Dec 2022
+                      </p>
+                      <p text-sm>
+                        Fullstack Designer | Software Developer | Blockchain
+                        Enthusiast
+                      </p>
                     </div>
-                    
                   </div>
                   <div className="grid w-full grid-cols-12 gap-4">
                     <div className="col-span-8 flex flex-col space-y-4 ">
-                      <p className="text-3xl font-bold text-gray-800 group-hover:underline decoration-indigo-6 00">
+                      <p className="decoration-indigo-6 00 text-3xl font-bold text-gray-800 group-hover:underline">
                         Lorem ipsum, dolor sit amet consectetur adipisicing
                         elit. Dolores, debitis!
                       </p>
-                      <p className="text-sm text-gray-500 break-words">
+                      <p className="break-words text-sm text-gray-500">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit. Ab odit quibusdam quasi ipsam praesentium expedita
                         non laborum temporibus eligendi odio neque libero magni
@@ -108,7 +115,7 @@ const Home: NextPage = () => {
                       </p>
                     </div>
                     <div className="col-span-4">
-                      <div className="h-full w-full rounded-xl bg-gray-300 transition hover:scale-105 duration-300 shadow-lg"></div>
+                      <div className="h-full w-full rounded-xl bg-gray-300 shadow-lg transition duration-300 hover:scale-105"></div>
                     </div>
                   </div>
                   <div>
@@ -129,8 +136,56 @@ const Home: NextPage = () => {
               ))}
             </div>
           </main>
-          <aside className="col-span-4 h-full w-full">
-            This is the aside part
+          <aside className="sticky top-36  col-span-4 flex h-full w-full flex-col space-y-4 p-6">
+            <div>
+              <h3 className="my-6 text-lg font-semibold">
+                People you might be interested
+              </h3>
+              <div className="flex flex-col space-y-4">
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-row items-center space-x-5 "
+                  >
+                    <div className="h-10 w-10 flex-none rounded-full bg-gray-300 "></div>
+                    <div>
+                      <div className="text-sm font-bold text-gray-900">
+                        John Doe
+                      </div>
+                      <div className="text-xs">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Temporibus sint totam nesciunt.
+                      </div>
+                    </div>
+                    <div>
+                      <button className="flex items-center space-x-3 rounded border border-gray-500 px-4 py-2 hover:border-gray-900 hover:text-gray-900">
+                        Follow
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <h3 className="my-6 text-lg font-semibold">Your reading list</h3>
+            <div className="flex flex-col space-y-8 ">
+              {Array.from({ length: 4 }).map((_, i) => (
+                <div key={i} className="flex items-center space-x-6 group">
+                  <div className="h-full w-2/5 aspect-square rounded-xl bg-gray-300"></div>
+                  <div className="flex w-3/5 flex-col space-y-2">
+                    <h3 className="text-lg font-semibold group-hover:underline decoration-indigo-600">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <div>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Libero, similique?
+                    </div>
+                    <div className="flex space-x-4 items-center w-full ">
+                     <div className="w-8 h-8 bg-gray-300 rounded-full"></div>
+                     <div>Chizaa &#x2022; </div>
+                     <div>Dec 22 2022</div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </aside>
         </section>
       </div>
