52c19a6b6ecdad48d77db506a1de50f6913419f0
“dennohkim” <kimathidennis38@gmail.com>
2023-01-23 10:21:37 +0300

    build main section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,11 @@ const Home: NextPage = () => {
         <title>T3Stack Blog Site</title>
       </Head>
       <div className="flex h-screen w-full flex-col">
-        <header className="flex h-20   w-full flex-row justify-around items-center bg-white border-b-[1px] border-gray-300 ">
-          
+        <header className="flex h-20   w-full flex-row items-center justify-around border-b-[1px] border-gray-300 bg-white ">
           <div>
-            <IoReorderThreeOutline className="text-2xl text-gray-600"/>
+            <IoReorderThreeOutline className="text-2xl text-gray-600" />
           </div>
-          <div className="font-thin text-xl">Ultimate Blog app</div>
+          <div className="text-xl font-thin">Ultimate Blog app</div>
           <div className="flex items-center space-x-4">
             <div>
               <BsBell className="text-2xl text-gray-600" />
@@ -25,16 +24,19 @@ const Home: NextPage = () => {
               <div className="h-5 w-5 rounded-full bg-gray-600" />
             </div>
             <div>
-              <button className="flex rounded items-center px-4 py-2 space-x-3 border border-gray-200 hover:border-gray-900 hover:text-gray-900">
+              <button className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 hover:border-gray-900 hover:text-gray-900">
                 <div>Write</div>
                 <div>
-                  <FiEdit className="text-2xl text-gray-600 "/>
+                  <FiEdit className="text-2xl text-gray-600 " />
                 </div>
               </button>
             </div>
           </div>
         </header>
-        <div className="grid grid-cols-12">{/* Main screen */}</div>
+        <section className="grid grid-cols-12 place-items-center w-full h-full">
+          <main className="col-span-8 w-full border-r border-gray-300 h-full">This is the main part</main>
+          <aside className="col-span-4 w-full h-full">This is the aside part</aside>
+        </section>
       </div>
     </>
   );
