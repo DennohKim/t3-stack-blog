ddefe2600011df39cafdc9e8ff5f9008dacc56ca
DennohKim <kimathidennis38@gmail.com>
2023-01-25 22:22:57 +0300

    feat: implement sign put functionality

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,14 +1,14 @@
-
 import React from "react";
 import { BsBell } from "react-icons/bs";
 import { FiEdit } from "react-icons/fi";
 import { IoReorderThreeOutline } from "react-icons/io5";
-import { useSession, signIn } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { HiLogout } from "react-icons/hi";
 
 const MainLayout = ({ children }: React.PropsWithChildren) => {
   const { data: sessionData, status } = useSession();
 
-  console.log(sessionData)
+  console.log(sessionData);
   return (
     <div className="flex h-full w-full flex-col">
       <header className="flex h-32   w-full flex-row items-center justify-around border-b-[1px] border-gray-300 bg-white ">
@@ -32,10 +32,21 @@ const MainLayout = ({ children }: React.PropsWithChildren) => {
                 </div>
               </button>
             </div>
+            <div>
+              <button onClick={() => signOut()} className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 hover:border-gray-900 hover:text-gray-900">
+                <div>Logout</div>
+                <div>
+                  <HiLogout className="text-2xl text-gray-600 " />
+                </div>
+              </button>
+            </div>
           </div>
         ) : (
           <div>
-            <button onClick={() => signIn()} className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 hover:border-gray-900 hover:text-gray-900">
+            <button
+              onClick={() => signIn()}
+              className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 hover:border-gray-900 hover:text-gray-900"
+            >
               Sign in with Google
             </button>
           </div>
